8925728da9f6bb5a0a3da0ef3d74e70c9f845615
parimarjan <parimarjannegi@gmail.com>
2019-07-02 05:54:45 +0000

    added finished job queries

---
+++ templates/myjob-test/6b.sql
@@ -0,0 +1,22 @@
+SELECT COUNT(*)
+FROM cast_info AS ci,
+     keyword AS k,
+     movie_keyword AS mk,
+     name AS n,
+     title AS t
+WHERE k.keyword IN ('superhero',
+                    'sequel',
+                    'second-part',
+                    'marvel-comics',
+                    'based-on-comic',
+                    'tv-special',
+                    'fight',
+                    'violence')
+  AND n.name LIKE '%Downey%Robert%'
+  AND t.production_year > 2014
+  AND k.id = mk.keyword_id
+  AND t.id = mk.movie_id
+  AND t.id = ci.movie_id
+  AND ci.movie_id = mk.movie_id
+  AND n.id = ci.person_id;
+
