fixed join loss computation bug

cardinality_estimation/algs.py
@@ -14,6 +14,7 @@ import pandas as pd
 import json
 from multiprocessing import Pool
 from pgm import PGM
+import park
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
@@ -588,7 +589,8 @@ class NN1(CardinalityEstimationAlg):
         # TODO: configure other variables
         self.max_iter = kwargs["max_iter"]
 
-    def train(self, db, training_samples, use_subqueries=False):
+    def train(self, db, training_samples, save_model=True,
+            use_subqueries=False):
         self.db = db
         if use_subqueries:
             training_samples = get_all_subqueries(training_samples)
@@ -622,11 +624,12 @@ class NN1(CardinalityEstimationAlg):
         net = SimpleRegression(len(X[0]),
                 int(len(X[0])*self.hidden_layer_multiple), 1)
 
-        make_dir("./models")
-        model_path = "./models/" + "nn1" + str(deterministic_hash(query_str))[0:5]
-        if os.path.exists(model_path):
-            net.load_state_dict(torch.load(model_path))
-            print("loaded trained model!")
+        if save_model:
+            make_dir("./models")
+            model_path = "./models/" + "nn1" + str(deterministic_hash(query_str))[0:5]
+            if os.path.exists(model_path):
+                net.load_state_dict(torch.load(model_path))
+                print("loaded trained model!")
 
         loss_func = qloss_torch
         # loss_func = rel_loss_torch
@@ -637,8 +640,9 @@ class NN1(CardinalityEstimationAlg):
 
         self.net = net
 
-        print("saved model path")
-        torch.save(net.state_dict(), model_path)
+        if save_model:
+            print("saved model path")
+            torch.save(net.state_dict(), model_path)
 
     def test(self, test_samples):
         X = []
@@ -658,7 +662,7 @@ class NN1(CardinalityEstimationAlg):
         else:
             pred = self.net(X)
             pred = pred.squeeze(1)
-        return pred.detach().numpy()
+        return pred.cpu().detach().numpy()
 
     def size(self):
         pass
@@ -702,20 +706,18 @@ class NN2(CardinalityEstimationAlg):
         # loss_func = rel_loss_torch
         print("feature len: ", len(features))
 
-        self._train_nn_join_loss(net, training_samples, loss_func=loss_func, max_iter=self.max_iter,
+        self.net = net
+        self._train_nn_join_loss(self.net, training_samples, loss_func=loss_func, max_iter=self.max_iter,
                 tfboard_dir=None, lr=0.0001, adaptive_lr=True,
                 loss_threshold=2.0)
 
-        self.net = net
-
     def _train_nn_join_loss(self, net, training_samples,
-            lr=0.00001, max_iter=10000, mb_size=4,
-            loss_func=None, tfboard_dir=None, adaptive_lr=False,
-            min_lr=1e-17, loss_threshold=1.0):
+            lr=0.0001, max_iter=10000, mb_size=4,
+            loss_func=None, tfboard_dir=None, adaptive_lr=True,
+            min_lr=1e-17, loss_threshold=1.0, use_jl=False):
         '''
         TODO: explain
         '''
-        print("NN2 training!")
         if loss_func is None:
             assert False
             loss_func = torch.nn.MSELoss()
@@ -731,21 +733,63 @@ class NN2(CardinalityEstimationAlg):
 
         optimizer = torch.optim.Adam(net.parameters(), lr=lr)
         # update learning rate
-        # if adaptive_lr:
-            # scheduler = ReduceLROnPlateau(optimizer, 'min', patience=20,
-                            # verbose=True, factor=0.1, eps=min_lr)
-            # plateau_min_lr = 0
+        if adaptive_lr:
+            scheduler = ReduceLROnPlateau(optimizer, 'min', patience=5,
+                            verbose=True, factor=0.1, eps=min_lr)
+            plateau_min_lr = 0
 
         num_iter = 0
+        # create a new park env, and close at the end.
+        env = park.make('query_optimizer')
+
+        X_all = []
+        Y_all = []
+        for si, sample in enumerate(training_samples):
+            X_all.append(sample.features)
+            Y_all.append(sample.true_sel)
+            for sq in sample.subqueries:
+                X_all.append(sq.features)
+                Y_all.append(sq.true_sel)
+        X = to_variable(X_all).float()
+        Y = to_variable(Y_all).float()
+
+        min_jl = 1.00
+        max_jl = None
+        min_qerr = 0.00
+        max_qerr = None
 
         while True:
+
+            if (num_iter % 100 == 0):
+                pred = net(X)
+                pred = pred.squeeze(1)
+                train_loss = loss_func(pred, Y)
+                if adaptive_lr:
+                    # FIXME: should we do this for minibatch / or for train loss?
+                    scheduler.step(train_loss)
+
+                jl = join_loss_nn(pred, training_samples, self, env)
+                # jl = join_loss_nn(pred, training_samples, self, None)
+                jl = np.mean(np.array(jl))
+
+                jl2 = compute_join_order_loss(self, training_samples,
+                        True)
+                jl2 = np.mean(jl2)
+                print("jl: ", jl)
+                print("jl2: ", np.mean(jl2))
+
+                pdb.set_trace()
+
+                print("num iter: {}, num samples: {}, loss: {}, join-loss {}".format(
+                    num_iter, len(X), train_loss.item(), jl2))
+
             idxs = np.random.choice(list(range(len(training_samples))), mb_size)
             xbatch = []
             ybatch = []
             mb_samples = []
             for si, sample in enumerate(training_samples):
-                mb_samples.append(sample)
                 if si in idxs:
+                    mb_samples.append(sample)
                     xbatch.append(sample.features)
                     ybatch.append(sample.true_sel)
                     for sq in sample.subqueries:
@@ -757,14 +801,27 @@ class NN2(CardinalityEstimationAlg):
 
             pred = net(xbatch)
             pred = pred.squeeze(1)
-            jl = join_loss_nn(pred, mb_samples, self)
-            jl = np.mean(np.array(jl))
             loss = loss_func(pred, ybatch)
-            loss = loss*jl
+            # if loss.item() < 10.00:
+                # jl = join_loss_nn(pred, mb_samples, self, env)
+                # jl = np.mean(np.array(jl))
+                # print("join loss: ", jl)
 
-            # if (num_iter % 100 == 0):
-            print("num iter: {}, num samples: {}, minibatch loss: {}".format(
-                num_iter, len(xbatch), loss.item()))
+            if (num_iter == 200 and use_jl):
+                jl = join_loss_nn(pred, mb_samples, self, env)
+                jl = np.mean(np.array(jl))
+                max_jl = jl
+                max_qerr = loss.item()
+
+            if (num_iter > 200 and use_jl):
+                jl = join_loss_nn(pred, mb_samples, self, env)
+                jl = np.mean(np.array(jl))
+
+                # normalize both of these to [0...1]
+                norm_jl = (jl - min_jl) / (max_jl - min_jl)
+                loss = (loss - min_qerr) / (max_qerr - min_qerr)
+                print("norm jl: {}, norm qerr: {}".format(norm_jl, loss.item()))
+                loss = loss * norm_jl
 
             if (num_iter > max_iter):
                 print("breaking because max iter done")
@@ -776,7 +833,6 @@ class NN2(CardinalityEstimationAlg):
             num_iter += 1
 
         print("done with training")
-        # print("training loss: ", train_loss)
 
     def test(self, test_samples):
         X = []
@@ -786,7 +842,7 @@ class NN2(CardinalityEstimationAlg):
         X = to_variable(X).float()
         pred = self.net(X)
         pred = pred.squeeze(1)
-        return pred.detach().numpy()
+        return pred.cpu().detach().numpy()
 
     def size(self):
         pass

cardinality_estimation/losses.py
@@ -93,6 +93,8 @@ def run_all_eps(env, fixed_agent=None):
         # query = deterministic_hash(query)
         query = env.get_current_query_name()
         if query in queries.keys():
+            # FIXME: ugly hack, so we don't leave an episode hanging midway
+            env._run_random_episode()
             break
         # episode loop
         num_ep = 0
@@ -139,22 +141,19 @@ def update_cards(est_cards, q):
         cards[table_key] = int(est_cards[j])
     return cards
 
-def join_loss_nn(pred, queries, alg, baseline="EXHAUSTIVE"):
+def join_loss_nn(pred, queries, alg, env,
+        baseline="EXHAUSTIVE"):
     '''
     TODO: also updates each query object with the relevant stats that we want
     to plot.
     '''
+    if env is None:
+        env = park.make('query_optimizer')
+
+    start = time.time()
     assert len(queries[0].subqueries) > 0
-    # create a new park env, and close at the end.
-    env = park.make('query_optimizer')
     # Set queries
     query_dict = {}
-
-    # TMP: debugging, hardcoded-queries
-    # queries = queries[0:1]
-    # fname = "/home/pari/query-optimizer/simple-queries/0.sql"
-    # with open(fname, "r") as f:
-        # queries[0].query = f.read()
 
     # each queries index is set to its name
     for i, q in enumerate(queries):
@@ -162,29 +161,27 @@ def join_loss_nn(pred, queries, alg, baseline="EXHAUSTIVE"):
 
     env.initialize_queries(query_dict)
     cardinalities = {}
-    # Set estimated cardinalities
-    pred_start = 1
+    # Set estimated cardinalities. For estimated cardinalities, we need to
+    # add ONLY the subquery cardinalities
+    pred_start = 0
     for i, q in enumerate(queries):
-        # yhat = alg.test(q.subqueries)
+        # skip the first query, since that is not a subquery
+        pred_start += 1
         yhat = []
+        # this loop depends on the fact that pred[0],
+        # pred[0+len(q[0].subqueries)]], etc would be the cardinalities for the
+        # full query objects
         for j in range(pred_start, pred_start+len(q.subqueries), 1):
             yhat.append(pred[j])
         yhat = np.array(yhat, dtype=np.float32)
+        assert len(yhat) == len(q.subqueries)
         totals = np.array([q.total_count for q in q.subqueries],
                         dtype=np.float32)
         est_cards = np.multiply(yhat, totals)
         cardinalities[str(i)] = update_cards(est_cards, q)
-
-        if not hasattr(q, "subq_cards"):
-            q.subq_cards = {}
-        q.subq_cards[alg.__str__()] = cardinalities[str(i)]
+        pred_start += len(q.subqueries)
 
     env.initialize_cardinalities(cardinalities)
-    # let us now initialize the cost model costs, based on all the subqueries that we
-    # have.
-    # cost_model = {}
-    # for i, q in enumerate(queries):
-        # cost_model[i] = update_cost_model(q)
 
     # Learn optimal agent for estimated cardinalities
     agents = []
@@ -204,8 +201,6 @@ def join_loss_nn(pred, queries, alg, baseline="EXHAUSTIVE"):
     for i, q in enumerate(queries):
         est_cards = np.array([q.true_count for q in q.subqueries])
         cardinalities[str(i)] = update_cards(est_cards, q)
-        # for later plotting
-        q.subq_cards["true"] = cardinalities[str(i)]
 
     env.initialize_cardinalities(cardinalities)
 
@@ -221,21 +216,18 @@ def join_loss_nn(pred, queries, alg, baseline="EXHAUSTIVE"):
     est_card_costs = []
 
     for i, q in enumerate(queries):
-        if not hasattr(q, "join_info"):
-            q.join_info = {}
         info = test_q[str(i)]
-        q.join_info[alg.__str__()] = info
         bcost = info["costs"][baseline]
         card_cost = info["costs"]["RL"]
         cur_error = card_cost - bcost
         total_error += card_cost - bcost
         baseline_costs.append(float(bcost))
         est_card_costs.append(float(card_cost))
-    print(q.join_info.keys())
-    total_avg_err = np.mean(np.array(est_card_costs)-np.array(baseline_costs))
-    print("total avg error: {}: {}".format(baseline, total_avg_err))
+
+    # total_avg_err = np.mean(np.array(est_card_costs)-np.array(baseline_costs))
     rel_errors = np.array(est_card_costs) / np.array(baseline_costs)
 
+    # print("join loss compute took ", time.time() - start)
     return rel_errors
 
 def compute_join_order_loss(alg, queries, use_subqueries,
@@ -328,8 +320,7 @@ def compute_join_order_loss(alg, queries, use_subqueries,
         baseline_costs.append(float(bcost))
         est_card_costs.append(float(card_cost))
     print(q.join_info.keys())
-    total_avg_err = np.mean(np.array(est_card_costs)-np.array(baseline_costs))
-    print("total avg error: {}: {}".format(baseline, total_avg_err))
+    # total_avg_err = np.mean(np.array(est_card_costs)-np.array(baseline_costs))
     rel_errors = np.array(est_card_costs) / np.array(baseline_costs)
 
     return rel_errors

utils/net.py
@@ -2,6 +2,8 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 class SimpleRegression(torch.nn.Module):
     # TODO: add more stuff?
     def __init__(self, n_input, n_hidden, n_output):
@@ -9,22 +11,22 @@ class SimpleRegression(torch.nn.Module):
         self.layer1 = nn.Sequential(
             nn.Linear(n_input, n_hidden, bias=True),
             nn.LeakyReLU()
-        )
+        ).to(device)
 
         # self.layer2 = nn.Sequential(
             # nn.Linear(n_hidden, n_output, bias=True),
             # nn.Sigmoid()
-        # )
+        # ).to(device)
 
         self.layer2 = nn.Sequential(
             nn.Linear(n_hidden, n_hidden, bias=True),
             nn.LeakyReLU()
-        )
+        ).to(device)
 
         self.layer3 = nn.Sequential(
             nn.Linear(n_hidden, n_output, bias=True),
             nn.Sigmoid()
-        )
+        ).to(device)
 
     def forward(self, x):
         output = self.layer1(x)

utils/utils.py
@@ -15,6 +15,8 @@ from utils.tf_summaries import TensorboardSummaries
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 import time
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 def is_float(val):
     try:
         float(val)
@@ -91,12 +93,15 @@ def to_variable(arr, use_cuda=True):
     if isinstance(arr, list) or isinstance(arr, tuple):
         arr = np.array(arr)
     if isinstance(arr, np.ndarray):
-        arr = Variable(torch.from_numpy(arr), requires_grad=True)
+        arr = Variable(torch.from_numpy(arr), requires_grad=True).to(device)
     else:
-        arr = Variable(arr, requires_grad=True)
+        arr = Variable(arr, requires_grad=True).to(device)
 
-    if torch.cuda.is_available() and use_cuda:
-        arr = arr.cuda()
+    # if torch.cuda.is_available() and use_cuda:
+        # print("returning cuda array!")
+        # arr = arr.cuda()
+    # else:
+        # pdb.set_trace()
     return arr
 
 
@@ -185,7 +190,7 @@ def train_nn(net, X, Y, lr=0.00001, max_iter=10000, mb_size=32,
     optimizer = torch.optim.Adam(net.parameters(), lr=lr)
     # update learning rate
     if adaptive_lr:
-        scheduler = ReduceLROnPlateau(optimizer, 'min', patience=3,
+        scheduler = ReduceLROnPlateau(optimizer, 'min', patience=20,
                         verbose=True, factor=0.1, eps=min_lr)
         plateau_min_lr = 0
 
@@ -194,7 +199,7 @@ def train_nn(net, X, Y, lr=0.00001, max_iter=10000, mb_size=32,
     Y = np.array(Y)
 
     while True:
-        if (num_iter % 1000 == 0):
+        if (num_iter % 100 == 0):
             # test on the full train set
             xbatch = X
             xbatch = to_variable(xbatch).float()