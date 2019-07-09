0e643918ed9aa1e78db1e60fbb286622f21726d5
parimarjan <parimarjannegi@gmail.com>
2019-07-09 09:29:52 +0000

    simple run test file

---
+++ run-adam-nojl.sh
@@ -0,0 +1,31 @@
+time python3 main.py --db_name imdb --template_dir templates/myjob -n 1 \
+--algs nn2 --test 0 --use_subqueries 1 --only_nonzero_samples 0 \
+--losses qerr,join-loss --qopt_java_output 0 --qopt_left_deep 1 \
+--qopt_reward_normalization "" --qopt_exh 0 --qopt_recompute_fixed_planners 1 \
+--baseline_join_alg "LEFT_DEEP" --result_dir \
+"adam-nojl2" --max_iter 25000 --use_jl 0 --qopt_verbose 1 \
+--qopt_log_file "./java2.log" --optimizer_name adam --eval_iter 500 \
+
+time python3 main.py --db_name imdb --template_dir templates/myjob -n 1 \
+--algs nn2 --test 0 --use_subqueries 1 --only_nonzero_samples 0 \
+--losses qerr,join-loss --qopt_java_output 0 --qopt_left_deep 1 \
+--qopt_reward_normalization "" --qopt_exh 0 --qopt_recompute_fixed_planners 1 \
+--baseline_join_alg "LEFT_DEEP" --result_dir \
+"adam-nojl3" --max_iter 25000 --use_jl 0 --qopt_verbose 1 \
+--qopt_log_file "./java2.log" --optimizer_name adam --eval_iter 500 \
+
+time python3 main.py --db_name imdb --template_dir templates/myjob -n 1 \
+--algs nn2 --test 0 --use_subqueries 1 --only_nonzero_samples 0 \
+--losses qerr,join-loss --qopt_java_output 0 --qopt_left_deep 1 \
+--qopt_reward_normalization "" --qopt_exh 0 --qopt_recompute_fixed_planners 1 \
+--baseline_join_alg "LEFT_DEEP" --result_dir \
+"adam-nojl4" --max_iter 25000 --use_jl 0 --qopt_verbose 1 \
+--qopt_log_file "./java2.log" --optimizer_name adam --eval_iter 500 \
+
+time python3 main.py --db_name imdb --template_dir templates/myjob -n 1 \
+--algs nn2 --test 0 --use_subqueries 1 --only_nonzero_samples 0 \
+--losses qerr,join-loss --qopt_java_output 0 --qopt_left_deep 1 \
+--qopt_reward_normalization "" --qopt_exh 0 --qopt_recompute_fixed_planners 1 \
+--baseline_join_alg "LEFT_DEEP" --result_dir \
+"adam-nojl5" --max_iter 25000 --use_jl 0 --qopt_verbose 1 \
+--qopt_log_file "./java2.log" --optimizer_name adam --eval_iter 500 \
