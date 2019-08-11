b2e81231c620e6edce1b11dcf0cccc007424f7ea
parimarjan <parimarjannegi@gmail.com>
2019-08-11 19:08:27 -0400

    added power basic template

---
+++ templates/power/all.sql
@@ -0,0 +1,21 @@
+SELECT COUNT(*)
+FROM
+power
+WHERE 'Xdate' < power.date
+AND power.date <= 'Ydate'
+AND 'Xtime' < power.time
+AND power.time <= 'Ytime'
+AND 'Xglobal_active_power' < power.global_active_power
+AND power.global_active_power <= 'Yglobal_active_power'
+AND 'Xglobal_reactive_power' < power.global_reactive_power
+AND power.global_reactive_power <= 'Yglobal_reactive_power'
+AND 'Xglobal_voltage' < power.global_voltage
+AND power.global_voltage <= 'Yglobal_voltage'
+AND 'Xglobal_intensity' < power.global_intensity
+AND power.global_intensity <= 'Yglobal_intensity'
+AND 'Xsub_metering_1' < power.sub_metering_1
+AND power.sub_metering_1 <= 'Ysub_metering_1'
+AND 'Xsub_metering_2' < power.sub_metering_2
+AND power.sub_metering_2 <= 'Ysub_metering_2'
+AND 'Xsub_metering_3' < power.sub_metering_3
+AND power.sub_metering_3 <= 'Ysub_metering_3'
