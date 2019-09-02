98615503966251c64ecd1bdcd5cf55e6175be640
parimarjan <parimarjannegi@gmail.com>
2019-09-02 22:59:58 +0000

    imdb predicate based indices

---
+++ data_scripts/imdb_join_gen_indices.sql
@@ -0,0 +1,4 @@
+CREATE INDEX IF NOT EXISTS kt_kind ON kind_type (kind);
+CREATE INDEX IF NOT EXISTS rt_role ON role_type (role);
+CREATE INDEX IF NOT EXISTS t_production_year ON title (production_year);
+CREATE INDEX IF NOT EXISTS n_gender ON name (gender);
