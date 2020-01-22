8723634ae520cbe75780a697041294474eab3456
parimarjan <parimarjannegi@gmail.com>
2020-01-22 04:18:38 -0500

    timeout no

--- get_runtimes.py
+++ get_runtimes.py
@@ -50,7 +50,7 @@ def execute_sql(sql, template="sql"):
     cursor.execute("SET geqo_threshold = {}".format(20))
     cursor.execute("SET join_collapse_limit = {}".format(16))
     cursor.execute("SET from_collapse_limit = {}".format(16))
-    cursor.execute("SET statement_timeout = {}".format(900000))
+    # cursor.execute("SET statement_timeout = {}".format(900000))
 
     start = time.time()
 
