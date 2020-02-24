a2645ac1a7eabad6957ccaa0c9493e78f9265d5f
parimarjan <parimarjannegi@gmail.com>
2020-02-24 06:34:52 +0000

    minor wut

--- test.sh
+++ test.sh
@@ -1,36 +1,14 @@
 
-python3 main.py --db_name imdb -n -1 --algs nn --qopt_use_java 0 --test 1 \
---result_dir plan_viz_results --eval_epoch 2 \
---sampling_priority_alpha 2.0 --eval_epoch_jerr 2 \
---num_hidden_layers 1 --max_discrete_featurizing_buckets 1 \
---hidden_layer_size 128 \
---reprioritize_epoch 2 \
---heuristic_features 1 --nn_type mscn --avg_jl_priority 1 \
---eval_test_while_training 1 --lr 0.0001 --max_epochs 40 \
---priority_err_divide_len 0 --exp_prefix planViz \
---prioritize_epoch 2
+python3 main.py --db_name imdb --query_dir our_dataset/queries/ -n -1 --algs nn \
+--losses qerr,join-loss  --test 1 --max_epochs 40 --tfboard 1 \
+--sampling_priority_type query --prioritize_epoch 1 \
+--reprioritize_epoch 1 --result_dir subq_loss_cmp_results \
+--sampling_priority_alpha 2.0 \
+--max_discrete_featurizing_buckets 1 --exp_prefix repr1 \
 
-python3 main.py --db_name imdb -n -1 --algs nn --qopt_use_java 0 --test 1 \
---result_dir plan_viz_results --eval_epoch 2 \
---sampling_priority_alpha 0.0 --eval_epoch_jerr 2 \
---num_hidden_layers 1 --max_discrete_featurizing_buckets 1 \
---hidden_layer_size 128 \
---reprioritize_epoch 2 \
---heuristic_features 1 --nn_type mscn --avg_jl_priority 1 \
---eval_test_while_training 1 --lr 0.0001 --max_epochs 40 \
---priority_err_divide_len 0 --exp_prefix planViz \
---prioritize_epoch 2
-
-# subquery priority example
-#python3 main.py --db_name imdb -n -1 --algs nn --qopt_use_java 0 --test 1 \
-#--result_dir plan_comparison_results --eval_epoch 2 \
-#--sampling_priority_alpha 2.0 --eval_epoch_jerr 2 \
-#--num_hidden_layers 1 --max_discrete_featurizing_buckets 1 \
-#--hidden_layer_size 32 \
-#--reprioritize_epoch 2 \
-#--heuristic_features 1 --nn_type mscn --avg_jl_priority 1 \
-#--eval_test_while_training 1 --lr 0.0001 --max_epochs 40 \
-#--priority_err_divide_len 0 --exp_prefix subQuerySamplingPriority \
-#--prioritize_epoch 1 --reprioritize_epoch 1000 \
-#--sampling_priority_type subquery
-
+python3 main.py --db_name imdb --query_dir our_dataset/queries/ -n -1 --algs nn \
+--losses qerr,join-loss  --test 1 --max_epochs 40 --tfboard 1 \
+--sampling_priority_type query --prioritize_epoch 1 \
+--sampling_priority_alpha 2.0 \
+--reprioritize_epoch 1000 --result_dir subq_loss_cmp_results \
+--max_discrete_featurizing_buckets 1 --exp_prefix noRepr \
