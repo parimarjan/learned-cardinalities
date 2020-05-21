important commit, fixes derivative calculations for nil8
* nil8 does reasonably well

cardinality_estimation/flow_loss.py
@@ -171,7 +171,7 @@ def get_costs_jax(card1, card2, card3, nilj, cost_model,
         # same as nli7 --> but consider the fact the right side of an index
         # nested loop join WILL not have predicates pushed down
         # also, remove the term for index entirely
-        if len(node1) == 1:
+        if nilj == 1:
             # using index on node1
             # nilj_cost = card2 + NILJ_CONSTANT*card1
             nilj_cost = card2
@@ -181,7 +181,7 @@ def get_costs_jax(card1, card2, card3, nilj, cost_model,
             joined_node_est = card3 * node1_selectivity
             nilj_cost += joined_node_est
 
-        elif len(node2) == 1:
+        elif nilj == 2:
             # using index on node2
             # nilj_cost = card1 + NILJ_CONSTANT*card2
             nilj_cost = card1
@@ -390,6 +390,7 @@ def single_forward2(yhat, totals, edges_head, edges_tail, edges_cost_node1,
         costs_grad = jacfwd(get_optimization_variables_jax, argnums=0)(yhat, totals, min_val,
                 max_val, normalization_type, edges_cost_node1, edges_cost_node2,
                 edges_head, nilj, cost_model, G, Q)
+        costs_grad = costs_grad.T
         print(costs_grad.shape, np.max(costs_grad))
 
         print("jax stuff took: ", time.time()-jax_start)
@@ -423,11 +424,11 @@ def single_forward2(yhat, totals, edges_head, edges_tail, edges_cost_node1,
             print("costs not close!")
             pdb.set_trace()
 
-        print(np.allclose(dgdxT2.T, costs_grad))
+        print(np.allclose(dgdxT2, costs_grad))
         print(np.min(dgdxT2), np.max(dgdxT2))
         print(np.min(costs_grad), np.max(costs_grad))
 
-        # pdb.set_trace()
+        pdb.set_trace()
 
     # min_est = np.min(est_cards.detach().numpy())
     # if min_est < 10.0: