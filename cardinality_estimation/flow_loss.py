cost model sanity, fixed sql-to-join-cost calculation
* before, we were not enforcing which type of scan will be chosen
* need to figure out a good formula for SeqScan (...)

cardinality_estimation/flow_loss.py
@@ -544,12 +544,6 @@ def single_forward2(yhat, totals, edges_head, edges_tail, edges_cost_node1,
 
         pdb.set_trace()
 
-    # min_est = np.min(est_cards.detach().numpy())
-    # if min_est < 10.0:
-        # print(min_est)
-        # print("min est low!")
-        # pdb.set_trace()
-
     Gv2 = np.zeros(len(totals))
     Gv2[final_node] = 1.0
 
@@ -562,11 +556,6 @@ def single_forward2(yhat, totals, edges_head, edges_tail, edges_cost_node1,
     v = invG @ Gv2 # vshape: Nx1
     v = v.detach().numpy()
 
-    # flows = Q2 @ v
-    # if np.min(flows) < 0.0:
-        # print(np.min(flows))
-        # pdb.set_trace()
-
     # TODO: we don't even need to compute the loss here if we don't want to
     loss2 = np.zeros(1, dtype=np.float32)
     assert Q2.dtype == np.float32
@@ -574,6 +563,7 @@ def single_forward2(yhat, totals, edges_head, edges_tail, edges_cost_node1,
     if isinstance(trueC_vec, torch.Tensor):
         trueC_vec = trueC_vec.detach().numpy()
     assert trueC_vec.dtype == np.float32
+    # just computes the loss
     fl_cpp.get_qvtqv(
             c_int(len(edges_head)),
             c_int(len(v)),
@@ -606,24 +596,6 @@ def single_backward(Q, invG,
             invG.ctypes.data_as(c_void_p),
             QinvG2.ctypes.data_as(c_void_p))
 
-    dfdg = np.zeros((len(edges_head), len(edges_head)), dtype=np.float32)
-    dfdg_start = time.time()
-    num_threads = int(len(edges_head) / 400)
-    num_threads = max(1, num_threads)
-    num_threads = min(20, num_threads)
-    fl_cpp.get_dfdg(
-            c_int(len(edges_head)),
-            c_int(len(v)),
-            edges_head.ctypes.data_as(c_void_p),
-            edges_tail.ctypes.data_as(c_void_p),
-            QinvG2.ctypes.data_as(c_void_p),
-            v.ctypes.data_as(c_void_p),
-            dfdg.ctypes.data_as(c_void_p),
-            c_int(num_threads))
-    print("dfdg took: ", time.time()-dfdg_start)
-
-    # dfdg = to_variable(dfdg).float()
-    # dfdg = to_variable(dfdg).float()
     if isinstance(trueC_vec, torch.Tensor):
         trueC_vec = trueC_vec.detach().numpy()
 
@@ -645,11 +617,31 @@ def single_backward(Q, invG,
             tQv.ctypes.data_as(c_void_p)
             )
     # print("tqv computations took: ", time.time()-tqv_start)
+    dCdg2 = np.zeros(tQv.shape, dtype=np.float32)
 
-    mat_start = time.time()
+    dfdg = np.zeros((len(edges_head), len(edges_head)), dtype=np.float32)
+    dfdg_start = time.time()
+    num_threads = int(len(edges_head) / 400)
+    num_threads = max(1, num_threads)
+    num_threads = min(20, num_threads)
+    fl_cpp.get_dfdg(
+            c_int(len(edges_head)),
+            c_int(len(v)),
+            edges_head.ctypes.data_as(c_void_p),
+            edges_tail.ctypes.data_as(c_void_p),
+            QinvG2.ctypes.data_as(c_void_p),
+            tQv.ctypes.data_as(c_void_p),
+            v.ctypes.data_as(c_void_p),
+            dfdg.ctypes.data_as(c_void_p),
+            dCdg2.ctypes.data_as(c_void_p),
+            c_int(num_threads))
+
+    # mat_start = time.time()
     # slow matrix mul when we have too many edges
-    dCdg = dfdg @ tQv
-    yhat_grad = dgdxT @ to_variable(dCdg).float()
+    # dCdg = dfdg @ tQv
+    # print("mat took: ", time.time()-mat_start)
+
+    yhat_grad = dgdxT @ to_variable(dCdg2).float()
 
     if normalize_flow_loss:
         yhat_grad /= opt_flow_loss

cardinality_estimation/losses.py
@@ -238,18 +238,22 @@ def compute_qerror(queries, preds, **kwargs):
 
     save_object(fn, df)
 
-    query_losses = {}
+    query_losses = defaultdict(list)
     query_idx = 0
     for sample in queries:
         template = sample["template_name"]
         cur_err = np.mean(errors[query_idx:query_idx+len(sample["subset_graph"].nodes())])
-        query_losses[sample["name"]] = cur_err
+        query_losses["name"].append(sample["name"])
+        query_losses["qerr"].append(cur_err)
         query_idx += len(sample["subset_graph"].nodes())
 
+    query_losses = pd.DataFrame(query_losses)
     qfn = rdir + "/" + "query_qerr.pkl"
     old_results = load_object(qfn)
     if old_results is not None:
         df = pd.concat([old_results, query_losses], ignore_index=True)
+    else:
+        df = query_losses
     save_object(qfn, df)
 
     return errors
@@ -369,34 +373,64 @@ def compute_join_order_loss(queries, preds, **kwargs):
 
     @output: updates ./results/join_order_loss.pkl file
     '''
+    def run_join_loss_exp(env, cost_model):
+        use_indexes = args.jl_indexes
+        exp_name = kwargs["exp_name"]
+        samples_type = kwargs["samples_type"]
+        pool = kwargs["pool"]
+
+        # here, we assume that the alg name is unique enough, for their results to
+        # be grouped together
+        rdir = RESULTS_DIR_TMP.format(RESULT_DIR = args.result_dir,
+                                       ALG = exp_name)
+        make_dir(rdir)
+        costs_fn = rdir + cost_model + "_jerr.pkl"
+        costs = load_object(costs_fn)
+        if costs is None:
+            columns = ["sql_key", "explain","plan","exec_sql","cost", "loss",
+                    "postgresql_conf", "samples_type", "template", "qfn"]
+            costs = pd.DataFrame(columns=columns)
+
+        cur_costs = defaultdict(list)
+        assert isinstance(costs, pd.DataFrame)
+
+        if args.jl_use_postgres:
+            est_costs, opt_costs, est_plans, opt_plans, est_sqls, opt_sqls = \
+                            join_loss_pg(sqls, join_graphs, true_cardinalities,
+                                    est_cardinalities, env, use_indexes, pdf=None,
+                                    pool = pool, join_loss_data_file =
+                                    args.join_loss_data_file)
+            losses = est_costs - opt_costs
+            for i, qrep in enumerate(queries):
+                sql_key = str(deterministic_hash(qrep["sql"]))
+                add_query_result_row(sql_key, samples_type,
+                        est_sqls[i], est_costs[i],
+                        losses[i],
+                        get_leading_hint(est_plans[i]),
+                        qrep["template_name"], cur_costs, costs,
+                        qrep["name"])
+        else:
+            print("TODO: add calcite based cost model")
+            assert False
+
+        cur_df = pd.DataFrame(cur_costs)
+        combined_df = pd.concat([costs, cur_df], ignore_index=True)
+        save_object(costs_fn, combined_df)
+
+        return est_costs, opt_costs
+
     assert isinstance(queries, list)
     assert isinstance(preds, list)
     assert isinstance(queries[0], dict)
 
     # env = park.make('query_optimizer')
     args = kwargs["args"]
+    alg_name = kwargs["name"]
     env = JoinLoss(args.cost_model, args.user, args.pwd, args.db_host,
             args.port, args.db_name)
-    use_indexes = args.jl_indexes
-    exp_name = kwargs["exp_name"]
-    samples_type = kwargs["samples_type"]
-    pool = kwargs["pool"]
-
-    # here, we assume that the alg name is unique enough, for their results to
-    # be grouped together
-    rdir = RESULTS_DIR_TMP.format(RESULT_DIR = args.result_dir,
-                                   ALG = exp_name)
-    make_dir(rdir)
-    costs_fn = rdir + "jerr.pkl"
-    costs = load_object(costs_fn)
-    if costs is None:
-        columns = ["sql_key", "explain","plan","exec_sql","cost", "loss",
-                "postgresql_conf", "samples_type", "template", "qfn"]
-        costs = pd.DataFrame(columns=columns)
-
-    cur_costs = defaultdict(list)
-
-    assert isinstance(costs, pd.DataFrame)
+    if "nested" in args.cost_model:
+        env2 = JoinLoss("cm1", args.user, args.pwd, args.db_host,
+                args.port, args.db_name)
 
     est_cardinalities = []
     true_cardinalities = []
@@ -425,30 +459,19 @@ def compute_join_order_loss(queries, preds, **kwargs):
         est_cardinalities.append(ests)
         true_cardinalities.append(trues)
 
-    if args.jl_use_postgres:
-        est_costs, opt_costs, est_plans, opt_plans, est_sqls, opt_sqls = \
-                        join_loss_pg(sqls, join_graphs, true_cardinalities,
-                                est_cardinalities, env, use_indexes, pdf=None,
-                                pool = pool, join_loss_data_file =
-                                args.join_loss_data_file)
-        losses = est_costs - opt_costs
-        for i, qrep in enumerate(queries):
-            sql_key = str(deterministic_hash(qrep["sql"]))
-            add_query_result_row(sql_key, samples_type,
-                    est_sqls[i], est_costs[i],
-                    losses[i],
-                    get_leading_hint(est_plans[i]),
-                    qrep["template_name"], cur_costs, costs,
-                    qrep["name"])
-    else:
-        print("TODO: add calcite based cost model")
-        assert False
+    est_costs, opt_costs = run_join_loss_exp(env, args.cost_model)
+    if "nested" in args.cost_model:
+        est_costs2, opt_costs2 = run_join_loss_exp(env2, "cm1")
+        losses2 = est_costs2 - opt_costs2
+        print("case: {}: alg: {}, samples: {}, {}: mean: {}, median: {}, 95p: {}, 99p: {}"\
+                .format(args.db_name, alg_name, len(queries),
+                    "join all",
+                    np.round(np.mean(losses2),3),
+                    np.round(np.median(losses2),3),
+                    np.round(np.percentile(losses2,95),3),
+                    np.round(np.percentile(losses2,99),3)))
 
-    cur_df = pd.DataFrame(cur_costs)
-    combined_df = pd.concat([costs, cur_df], ignore_index=True)
-    save_object(costs_fn, combined_df)
 
-    losses = np.array(est_costs) - np.array(opt_costs)
     return np.array(est_costs) - np.array(opt_costs)
 
 def compute_flow_loss(queries, preds, **kwargs):
@@ -557,6 +580,7 @@ def compute_plan_loss(queries, preds, **kwargs):
                 env.compute_loss(queries, preds, pool=pool,
                         true_cardinalities=true_cardinalities,
                         join_graphs=join_graphs)
+
     # save plan_pg_err results
     losses = est_costs - opt_costs
     losses_pg = est_costs_pg - opt_costs_pg

cardinality_estimation/nn.py
@@ -1286,7 +1286,7 @@ class NN(CardinalityEstimationAlg):
             cost_model_ratio = opt_plan_pg_costs / opt_costs
             print("cost model losses: ")
             print(np.mean(cost_model_losses), np.mean(cost_model_ratio))
-            pdb.set_trace()
+            # pdb.set_trace()
 
         if np.mean(join_losses) < self.best_join_loss \
                 and epoch > self.start_validation \